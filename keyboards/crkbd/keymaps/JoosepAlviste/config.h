be8fbfb745fccd352f2c96495c9bc8465df6d930
Joosep Alviste <joosep.alviste@gmail.com>
2021-09-12 18:58:48 +0300

    Configure crkbd mapping

--- keyboards/crkbd/keymaps/JoosepAlviste/config.h
+++ keyboards/crkbd/keymaps/JoosepAlviste/config.h
@@ -31,7 +31,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define USE_SERIAL_PD2
 
 #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 100
+#define TAPPING_TERM 200
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+// Apply the modifier on keys that are tapped during a short hold of a modtap
+// Try disabling if activating mods too often
+#define PERMISSIVE_HOLD
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD
+
+#define UNICODE_SELECTED_MODES UC_MAC
+#define UNICODE_KEY_MAC KC_RALT
 
 #ifdef RGBLIGHT_ENABLE
     #undef RGBLED_NUM
