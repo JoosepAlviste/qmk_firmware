985fd72da54bf78ccb7004f33d0575f772725f9b
Joosep Alviste <joosep.alviste@gmail.com>
2022-02-23 08:43:52 +0200

    Add grave and tilde characters

--- keyboards/crkbd/keymaps/JoosepAlvisteSecond/keymap.c
+++ keyboards/crkbd/keymaps/JoosepAlvisteSecond/keymap.c
@@ -42,8 +42,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [SYM] = LAYOUT_split_3x6_3(
         XXXXXXX, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,   KC_CIRC, KC_AMPR,  KC_ASTR, KC_LPRN, KC_RPRN, XXXXXXX,
-        XXXXXXX, KC_MINS, KC_UNDS, KC_EQL,  KC_PLUS, _______,   _______, OS_SHFT,  OS_CMD,  OS_ALT,  OS_CTRL, XXXXXXX,
-        XXXXXXX, KC_PIPE, KC_BSLS, KC_LBRC, KC_LCBR, _______,   _______, KC_RCBR,  KC_RBRC, KC_COLN, KC_SCLN, XXXXXXX,
+        XXXXXXX, KC_MINS, KC_UNDS, KC_EQL,  KC_PLUS, KC_GRAVE,  _______, OS_SHFT,  OS_CMD,  OS_ALT,  OS_CTRL, XXXXXXX,
+        XXXXXXX, KC_PIPE, KC_BSLS, KC_LBRC, KC_LCBR, KC_TILDE,  _______, KC_RCBR,  KC_RBRC, KC_COLN, KC_SCLN, XXXXXXX,
                                    _______, _______, _______,   _______, _______, _______
     ),
 
